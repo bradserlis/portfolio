5ce4b0b348222f4063d65074e9525cd69271df70
bradserlis <bradserlis@yahoo.com>
2018-06-14 14:00:24 -0700

    separating pages

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ console.log('sanity check');
   $(document).ready(function(){
     $('.parallax').parallax();
       M.AutoInit();
+
+    $('.carousel').carousel({
+    onCycleTo: function(data) {
+      console.log(data);
+      console.log($(data).index());
+    }
+});
+    $('.scrollspy').scrollSpy();
+
 })
