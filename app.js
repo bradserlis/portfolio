e0962cd1528c2107a249b9b7b2b931046ab856e5
bradserlis <bradserlis@yahoo.com>
2019-02-25 15:46:09 -0800

    update jammate post-launch

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
-console.log('sanity check');
+console.log("sanity check");
 
-  $(document).ready(function(){
-    $('.parallax').parallax();
-      M.AutoInit();
+$(document).ready(function() {
+  $(".parallax").parallax();
+  M.AutoInit();
 
-    $('.carousel').carousel({
+  $(".carousel").carousel({
     onCycleTo: function(data) {
       // console.log(data);
       // console.log($(data).index());
       show_project(data);
     }
+  });
+  $(".sidenav").sidenav();
+  $(".scrollspy").scrollSpy();
 });
-        $('.sidenav').sidenav();
-    $('.scrollspy').scrollSpy();
-
-})
 
 // reveal
 
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('#skills', { duration: 700 });
-sr.reveal('#whoami', { duration: 1500 });
-sr.reveal('#aboutme', { duration: 700 });
-sr.reveal('.section-title', { duration: 1200 });
-sr.reveal('#current-project', { duration: 1800 });
-sr.reveal('.current-project', { duration: 1500 });
-sr.reveal('.current-project-app-name', { duration: 1500 });
-sr.reveal('.band_photo', { duration: 1000 });
+sr.reveal("#skills", { duration: 700 });
+sr.reveal("#whoami", { duration: 1500 });
+sr.reveal("#aboutme", { duration: 700 });
+sr.reveal(".section-title", { duration: 1200 });
+sr.reveal("#current-project", { duration: 1800 });
+sr.reveal(".current-project", { duration: 1500 });
+sr.reveal(".appear", { duration: 1500 });
+sr.reveal(".current-project-app-name", { duration: 1500 });
+sr.reveal(".band_photo", { duration: 1000 });
 // sr.reveal('.row', { duration: 1000 });
-sr.reveal('.path_background', { duration: 1000 });
-sr.reveal('.carousel', { duration: 1000 });
-sr.reveal('.row1', { duration: 1500 });
-sr.reveal('.row2', { duration: 1500 });
-sr.reveal('.row3', { duration: 1500 });
-sr.reveal('.row4', { duration: 1500 });
+sr.reveal(".path_background", { duration: 1000 });
+sr.reveal(".carousel", { duration: 1000 });
+sr.reveal(".row1", { duration: 1500 });
+sr.reveal(".row2", { duration: 1500 });
+sr.reveal(".row3", { duration: 1500 });
+sr.reveal(".row4", { duration: 1500 });
 
-// === 
+// ===
 // typeit.js
 // ===
 
-var instance = new TypeIt('#whoami', {
-    // strings: ['This is my string!'],
-    //-- Other options...
+var instance = new TypeIt("#whoami", {
+  // strings: ['This is my string!'],
+  //-- Other options...
 });
 
-new TypeIt('#whoami', {
-    strings: ["Web Developer.", "Software Developer.", "Gamer.", "Musician.", "<span class='colorful-background'> Full Stack Developer.</span>"],
-    speed: 85,
-    breakLines: false,
-    autoStart: false
+new TypeIt("#whoami", {
+  strings: [
+    "Web Developer.",
+    "Software Developer.",
+    "Gamer.",
+    "Musician.",
+    "<span class='colorful-background'> Full Stack Developer.</span>"
+  ],
+  speed: 85,
+  breakLines: false,
+  autoStart: false
 });
 
-// 
+//
 
 // $(document).scroll(function() {
 //   var y = $(this).scrollTop();
@@ -62,42 +68,45 @@ new TypeIt('#whoami', {
 //   }
 // });
 
-const show_project = (data) => {
-  if($(data).index() === 0){
-    $('#ninja_arena').show()
-    $('#jammate').removeClass("scale-in")
-    $('#jammate').hide()
-    $('#tell_em').removeClass("scale-in")
-    $('#tell_em').hide()
-    $('#wtp').removeClass("scale-in")
-    $('#wtp').hide()
-    $('#ninja_arena').addClass("scale-in")
-  }if($(data).index()=== 1){
-    $('#jammate').show()
-    $('#ninja_arena').removeClass("scale-in")
-    $('#ninja_arena').hide()
-    $('#tell_em').removeClass("scale-in")
-    $('#tell_em').hide()
-    $('#wtp').removeClass("scale-in")
-    $('#wtp').hide()
-    $('#jammate').addClass("scale-in")
-  }if($(data).index()===2){
-    $('#tell_em').show()
-    $('#jammate').removeClass("scale-in")
-    $('#jammate').hide()
-    $('#ninja_arena').removeClass("scale-in")
-    $('#ninja_arena').hide()
-    $('#wtp').removeClass("scale-in")
-    $('#wtp').hide() 
-    $('#tell_em').addClass("scale-in")
-  }if($(data).index()===3){
-    $('#wtp').show()
-    $('#jammate').removeClass("scale-in")
-    $('#jammate').hide()
-    $('#ninja_arena').removeClass("scale-in")
-    $('#ninja_arena').hide()
-    $('#tell_em').removeClass("scale-in")
-    $('#tell_em').hide() 
-    $('#wtp').addClass("scale-in")
+const show_project = data => {
+  if ($(data).index() === 0) {
+    $("#ninja_arena").show();
+    $("#jammate").removeClass("scale-in");
+    $("#jammate").hide();
+    $("#tell_em").removeClass("scale-in");
+    $("#tell_em").hide();
+    $("#wtp").removeClass("scale-in");
+    $("#wtp").hide();
+    $("#ninja_arena").addClass("scale-in");
   }
-}
+  if ($(data).index() === 1) {
+    $("#jammate").show();
+    $("#ninja_arena").removeClass("scale-in");
+    $("#ninja_arena").hide();
+    $("#tell_em").removeClass("scale-in");
+    $("#tell_em").hide();
+    $("#wtp").removeClass("scale-in");
+    $("#wtp").hide();
+    $("#jammate").addClass("scale-in");
+  }
+  if ($(data).index() === 2) {
+    $("#tell_em").show();
+    $("#jammate").removeClass("scale-in");
+    $("#jammate").hide();
+    $("#ninja_arena").removeClass("scale-in");
+    $("#ninja_arena").hide();
+    $("#wtp").removeClass("scale-in");
+    $("#wtp").hide();
+    $("#tell_em").addClass("scale-in");
+  }
+  if ($(data).index() === 3) {
+    $("#wtp").show();
+    $("#jammate").removeClass("scale-in");
+    $("#jammate").hide();
+    $("#ninja_arena").removeClass("scale-in");
+    $("#ninja_arena").hide();
+    $("#tell_em").removeClass("scale-in");
+    $("#tell_em").hide();
+    $("#wtp").addClass("scale-in");
+  }
+};
